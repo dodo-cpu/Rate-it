2276c4591ccf6c4cdccf8a007aab4272f0a973c1
aitgoehner <63348301+aitgoehner@users.noreply.github.com>
2020-04-15 16:28:50 +0200

    Adding Model classes
    added some of the Model classes and their structure

---
+++ Models/Category.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rateit.Models
+{
+    class Category
+    {
+
+        #region Propertiies
+
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        public int ParentId { get; private set; }
+
+        #endregion
+
+        #region public Methods
+
+        public Category(int id)
+        {
+            this.Id = id;
+
+            LoadData();
+        }
+
+        #endregion
+
+        #region private methods
+
+        /// <summary>
+        /// Reads the data from the database
+        /// </summary>
+        private void LoadData()
+        {
+            //TODO: DB Get
+        }
+
+        #endregion
+
+    }
+}
---
+++ Models/Topic.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rateit.Models
+{
+    class Topic
+    {
+
+        #region Properties
+
+        public int Id { get; private set; }
+
+        public string Name { get; private set; }
+
+        private List<Criterion> Criteria { get; set; }
+
+        public int UserId { get; private set; }
+
+        public int CategoryId { get; private set; }
+
+        #endregion
+
+        #region public methods
+
+        public Topic(int id, int userId)
+        {
+            this.Id = id;
+            this.UserId = userId;
+
+            LoadData();
+        }
+
+        /// <summary>
+        /// Returns the ratings of the Topic made from the public
+        /// </summary>
+        /// <returns></returns>
+        public List<double> GetPublicRatings()
+        {
+            List<double> ratings = new List<double>();
+
+            foreach (Criterion criterion in this.Criteria)
+            {
+                ratings.Add(criterion.publicRating);
+            }
+
+            return ratings;
+        }
+
+        /// <summary>
+        /// Returns the ratings of the Topic made from the current user
+        /// </summary>
+        /// <returns></returns>
+        public List<double> GetUserRatings()
+        {
+            List<double> ratings = new List<double>();
+
+            foreach (Criterion criterion in this.Criteria)
+            {
+                ratings.Add(criterion.userRating);
+            }
+
+            return ratings;
+        }
+
+        /// <summary>
+        /// writes the user ratings to the Database
+        /// </summary>
+        /// <param name="userRatings">the users ratings of the Criteria</param>
+        /// <returns></returns>
+        public bool Rate(List<double> userRatings)
+        {
+            for (int i = 0; i < userRatings.Count; i++)
+            {
+              this.Criteria[i].Rate(userRatings[i]);
+            }
+
+            //TODO return
+        }
+
+        #endregion
+
+        #region private methods
+
+        /// <summary>
+        /// Loads the Data from the Database
+        /// </summary>
+        private void LoadData()
+        {
+            //TODO: DB Get
+        }
+
+        #endregion
+
+    }
+}
