cceb52165d5260b9bc3ca4c412b55dc6226f6b3a
shrshr01 <63853804+shrshr01@users.noreply.github.com>
2020-04-28 12:59:52 +0200

    EditView

---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            Window1 signUp = new Window1();
+            //signUp.Show();
+            signUp.Owner = this;
+            this.Hide();
+            signUp.ShowDialog();
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            Window2 ranking = new Window2();
+            //ranking.Show();
+            ranking.Owner = this;
+            this.Hide();
+            ranking.ShowDialog();
+           
+        }
+
+       
+    }
+}
