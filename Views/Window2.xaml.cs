b5393acfc622029af36b2c66e66c77acae0b9945
shrshr01 <63853804+shrshr01@users.noreply.github.com>
2020-04-28 12:59:52 +0200

    EditView

---
+++ Views/Window2.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaktionslogik für Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Application.Current.Shutdown();
+        }
+        public Window2()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void Buttonbuch_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Buttonfilm_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 rate = new Window3();
+            //signUp.Show();
+            rate.Owner = this;
+            this.Hide();
+            rate.ShowDialog();
+
+        }
+
+        private void Buttonsong_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Views/Window3.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaktionslogik für Window3.xaml
+    /// </summary>
+    public partial class Window3 : Window
+    {
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Application.Current.Shutdown();
+        }
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtSlider1.Text = slider1.Value.ToString();
+        }
+
+        private void txtSlider1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtSlider2.Text = slider2.Value.ToString();
+        }
+
+        private void txtSlider2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void slider3_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            txtSlider3.Text = slider3.Value.ToString();
+        }
+
+        private void txtSlider3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
