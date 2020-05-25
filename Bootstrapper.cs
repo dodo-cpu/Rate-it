b0b516ea26feb0ef5e5db152108bf703d13ea0c2
aitgoehner <63348301+aitgoehner@users.noreply.github.com>
2020-05-25 19:56:22 +0200

    Caliburn implemented
    -Caliburn.micro implemented
    -New ShellView created
    -Category, Topic debugged and connected

---
+++ Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using Rateit.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Rateit
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+    }
+}
--- Views/ShellView.xaml.cs
+++ Views/ShellView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Rateit.Views
 {
     /// <summary>
-    /// Interaktionslogik für LoginView.xaml
+    /// Interaktionslogik für ShellView.xaml
     /// </summary>
-    public partial class LoginView : UserControl
+    public partial class ShellView : Window
     {
-        public LoginView()
+        public ShellView()
         {
             InitializeComponent();
         }
