21e8cd0f015e4f25cf8b17851f3b4c71aed6a98f
aitgoehner <63348301+aitgoehner@users.noreply.github.com>
2020-05-26 21:37:37 +0200

    LoginView and Events implemented

---
+++ Events/AggregatorProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace Rateit.Events
+{
+    static class AggregatorProvider
+    {
+        // The event aggregator
+        public static EventAggregator Aggregator = new EventAggregator();
+    }
+}
--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -7,107 +7,139 @@ using System.ComponentModel;
 using System.Windows.Input;
 using System.Windows.Controls;
 using Caliburn.Micro;
+using Rateit.Events;
 
 namespace Rateit.ViewModels
 {
-    public class LoginViewModel : INotifyPropertyChanged
+    public class LoginViewModel
     {
+        private readonly IEventAggregator _eventAggregator;
+
+
 
         #region fields
 
-        public ICommand _loginCommand;
-
-        private ICommand _registerCommand;
+        private Models.User _user;
+        private string _name;
 
         #endregion
 
         #region properties
 
-        public string Name { private get; set; }
-
-        public Models.User User { get; private set; }
-
-        public ICommand LoginCommand
+        public Models.User User
         {
-            get
+            get { return _user; }
+            set { _user = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+
+        #endregion
+
+        #region public methods
+
+        public void Login()
+        {
+            User = Models.User.Login(Name, Password);
+            if (User != null)
             {
-                if (_loginCommand == null)
-                {
-                    _loginCommand = new Commands.RelayCommand(c => OnLogin(this.Name, c), c => CanLogin(),false);
-                }
-                return _loginCommand;
+                AggregatorProvider.Aggregator.PublishOnCurrentThread(new LoginEvent(User));
+            }
+            else
+            {
+                //TODO: signal to user didnt work
             }
         }
 
-        public ICommand RegisterCommand
+
+        public void Register()
         {
-            get
+            User = Models.User.Register(Name, Password);
+            if (User != null)
             {
-                if (_registerCommand == null)
-                {
-                    _registerCommand = new Commands.RelayCommand(c => OnRegister(this.Name, c), c => CanLogin(), false);
-                }
-                return _registerCommand;
+                AggregatorProvider.Aggregator.PublishOnCurrentThread(new LoginEvent(User));
             }
+            else
+            {
+                //TODO: signal to user didnt work
+            }
+        }
+
+        public void PasswordChanged(PasswordBox source)
+        {
+            Password = source.Password;
         }
 
         #endregion
 
         #region private methods
 
-        private void OnLogin(string name, object password)
-        {
-            PasswordBox pw = password as PasswordBox;
-            this.User = Models.User.Login(name, pw.Password);
-            if (this.User != null)
-            {
-                OnLoggedIn();
-            }
-            else
-            {
-                //TODO: Signal to the user that input was incorrect
-            }
-        }
+        //private void OnLogin(string name, object password)
+        //{
+        //    PasswordBox pw = password as PasswordBox;
+        //    this.User = Models.User.Login(name, pw.Password);
+        //    if (this.User != null)
+        //    {
+        //        OnLoggedIn();
+        //    }
+        //    else
+        //    {
+        //        //TODO: Signal to the user that input was incorrect
+        //    }
+        //}
 
-        private bool CanLogin()
-        {
-            //TODO: Input check / Exceptions
-            return true;
-        }
+        //private bool CanLogin()
+        //{
+        //    //TODO: Input check / Exceptions
+        //    return true;
+        //}
 
-        private void OnRegister(string name, object password)
-        {
-            PasswordBox pw = password as PasswordBox;
-            this.User = Models.User.Register(name, pw.Password);
-            if (this.User != null)
-            {
-                OnLoggedIn();
-            }
-            else
-            {
-                //TODO: Signal to the user that input was incorrect
-            }
-        }
+        //private void OnRegister(string name, object password)
+        //{
+        //    PasswordBox pw = password as PasswordBox;
+        //    this.User = Models.User.Register(name, pw.Password);
+        //    if (this.User != null)
+        //    {
+        //        OnLoggedIn();
+        //    }
+        //    else
+        //    {
+        //        //TODO: Signal to the user that input was incorrect
+        //    }
+        //}
 
         #endregion
 
         #region events and delegates
 
-        public event PropertyChangedEventHandler PropertyChanged;
+        //public event PropertyChangedEventHandler PropertyChanged;
 
-        public event LoggedInEventHandler LoggedIn;
+        //public event LoggedInEventHandler LoggedIn;
 
-        public delegate void LoggedInEventHandler(object source, EventArgs args);
+        //public delegate void LoggedInEventHandler(object source, EventArgs args);
 
-        private void RaisePropertyChanged(string property)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
-        }
+        //private void RaisePropertyChanged(string property)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        //}
 
-        protected virtual void OnLoggedIn()
-        {
-            LoggedIn?.Invoke(this, EventArgs.Empty);
-        }
+        //protected virtual void OnLoggedIn()
+        //{
+        //    LoggedIn?.Invoke(this, EventArgs.Empty);
+        //}
 
         #endregion
 
