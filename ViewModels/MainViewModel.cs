606788fdeddba49be93d74942b1ae60a7778affc
aitgoehner <63348301+aitgoehner@users.noreply.github.com>
2020-05-27 16:28:01 +0200

    MainView created, Multiple Views splitted and added as children of MainView

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -8,89 +8,151 @@ using System.Windows.Input;
 using System.Collections.ObjectModel;
 using Rateit.Views;
 using Rateit.Models;
+using Caliburn.Micro;
+using Rateit.Events;
 
 namespace Rateit.ViewModels
 {
-    class MainViewModel
-    {
+	public class MainViewModel : Conductor<object>
+	{
 
-        #region fields
+		#region Fields
 
-        private ICommand _buttonCommand;
-        private ObservableCollection<Category> _parentCategories;
-        private ObservableCollection<Category> _childCategories;
-        private ObservableCollection<Topic> _topics;
+		private User _user;
+		private BindableCollection<Category> _parentCategories = new BindableCollection<Category>();
+		private Category _selectedParentCategory;
+		private BindableCollection<Category> _childCategories = new BindableCollection<Category>();
+		private Category _selectedChildCategory;
+		private BindableCollection<Topic> _topics = new BindableCollection<Topic>();
+		private Topic _selectedTopic;
 
-        #endregion
+		#endregion
 
-        #region properties
+		#region Properties
 
-        public ICommand ButtonCommand
-        {
-            get
-            {
-                if (_buttonCommand == null)
-                {
-                    _buttonCommand = new Commands.RelayCommand(c => OnButton(), c => true, false);
-                }
-                return _buttonCommand;
-            }
-        }
+		public User User
+		{
+			get { return _user; }
+			set
+			{
+				_user = value;
+				NotifyOfPropertyChange(() => User);
+			}
+		}
 
-        private ObservableCollection<Category> ParentCategories { get => _parentCategories; set => _parentCategories = value; }
+		public BindableCollection<Category> ParentCategories
+		{
+			get { return _parentCategories; }
+			set { _parentCategories = value; }
+		}
 
-        private ObservableCollection<Category> ChildtCategories { get => _childCategories; set => _childCategories = value; }
+		public Category SelectedParentCategory
+		{
+			get { return _selectedParentCategory; }
+			set
+			{
+				_selectedParentCategory = value;
+				NotifyOfPropertyChange(() => SelectedParentCategory);
+				LoadChildCategories();
+				ActivateItem(new OverViewModel(SelectedParentCategory.Id));
+			}
+		}
 
-        private ObservableCollection<Topic> Topics { get => _topics; set => _topics = value; }
+		public BindableCollection<Category> ChildCategories
+		{
+			get { return _childCategories; }
+			set { _childCategories = value; }
+		}
 
-        #endregion
+		public Category SelectedChildCategory
+		{
+			get { return _selectedChildCategory; }
+			set
+			{
+				_selectedChildCategory = value;
+				NotifyOfPropertyChange(() => SelectedChildCategory);
+				LoadTopics();
+				ActivateItem(new OverViewModel(SelectedChildCategory.Id));
+			}
+		}
 
-        #region public methods
+		public BindableCollection<Topic> Topics
+		{
+			get { return _topics; }
+			set { _topics = value; }
+		}
 
-        public MainViewModel()
-        {
-            this.ParentCategories = new ObservableCollection<Category>(Category.GetCategoriesByParent(null));
-            //this.LoginView = new Views.LoginView();
-            //this.LoginView.Show();
-        }
+		public Topic SelectedTopic
+		{
+			get { return _selectedTopic; }
+			set { _selectedTopic = value; }
+		}
 
-        public void OnButton()
-        {
-            //RaiseHide();
-        }
+		#endregion
 
-        #endregion
+		#region Public Methods
 
-        #region private methods
+		public MainViewModel(User loggedInUser)
+		{
+			User = loggedInUser;
+			LoadParentCategories();
+			//TODO: specify starting category
+			ActivateItem(new OverViewModel(1));
+
+		}
+
+		public void Logout()
+		{
+			AggregatorProvider.Aggregator.PublishOnCurrentThread(new LogoutEvent());
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void LoadParentCategories()
+		{
+			ParentCategories.Clear();
+			List<Category> categories = Category.GetCategoriesByParent(null);
+			foreach (Category category in categories)
+			{
+				ParentCategories.Add(category);
+			}
+		}
+
+		private void LoadChildCategories()
+		{
+			ChildCategories.Clear();
+			if (SelectedParentCategory != null)
+			{
+				List<Category> categories = Category.GetCategoriesByParent(SelectedParentCategory.Id);
+				foreach (Category category in categories)
+				{
+					ChildCategories.Add(category);
+				}
+			}
+		}
+
+		private void LoadTopics()
+		{
+			Topics.Clear();
+			if (SelectedChildCategory != null)
+			{
+				List<Topic> topics = Topic.GetTopicsByCategoryId(SelectedChildCategory.Id);
+				foreach (Topic topic in topics)
+				{
+					Topics.Add(topic);
+				}
+			}
+		}
+
+		#endregion
+
+		#region Events
 
 
 
-        #endregion
+		#endregion
 
-        //#region events
-
-        //public event PropertyChangedEventHandler PropertyChanged;
-
-        //private void RaisePropertyChanged(string property)
-        //{
-        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
-        //}
-
-        //public event HideEventHandler Hiding;
-        //public delegate void HideEventHandler();
-        //private void RaiseHide()
-        //{
-        //    Hiding?.Invoke();
-        //}
-
-        //public event ShowEventHandler Showing;
-        //public delegate void ShowEventHandler();
-        //private void RaiseShow()
-        //{
-        //    Showing?.Invoke();
-        //}
-
-        //#endregion
-
-    }
+	}
 }
--- ViewModels/ShellViewModel.cs
+++ ViewModels/ShellViewModel.cs
@@ -9,91 +9,17 @@ using Rateit.Events;
 
 namespace Rateit.ViewModels
 {
-    public class ShellViewModel : Conductor<object>, IHandle<LoginEvent>
+    public class ShellViewModel : Conductor<object>, IHandle<LoginEvent>, IHandle<LogoutEvent>
     {
 
         #region Fields
 
-        private User _user;
-		private BindableCollection<Category> _parentCategories = new BindableCollection<Category>();
-		private Category _selectedParentCategory;
-		private BindableCollection<Category> _childCategories = new BindableCollection<Category>();
-		private Category _selectedChildCategory;
-		private BindableCollection<Topic> _topics = new BindableCollection<Topic>();
-		private Topic _selectedTopic;
-		private System.Windows.Visibility _logoutVisibility = System.Windows.Visibility.Hidden;
+
 
 		#endregion
 
 		#region Properties
 
-		public User User
-		{
-			get { return _user; }
-			set 
-			{ 
-				_user = value;
-				NotifyOfPropertyChange(() => User);
-			}
-		}
-
-		public BindableCollection<Category> ParentCategories
-		{
-			get { return _parentCategories; }
-			set { _parentCategories = value; }
-		}
-
-		public Category SelectedParentCategory
-		{
-			get { return _selectedParentCategory; }
-			set 
-			{ 
-				_selectedParentCategory = value;
-				NotifyOfPropertyChange(() => SelectedParentCategory);
-				LoadChildCategories();
-				ActivateItem(new OverViewModel(SelectedParentCategory.Id));
-			}
-		}
-
-		public BindableCollection<Category> ChildCategories
-		{
-			get { return _childCategories; }
-			set { _childCategories = value; }
-		}
-
-		public Category SelectedChildCategory
-		{
-			get { return _selectedChildCategory; }
-			set 
-			{ 
-				_selectedChildCategory = value;
-				NotifyOfPropertyChange(() => SelectedChildCategory);
-				LoadTopics();
-				ActivateItem(new OverViewModel(SelectedChildCategory.Id));
-			}
-		}
-
-		public BindableCollection<Topic> Topics
-		{
-			get { return _topics; }
-			set { _topics = value; }
-		}
-
-		public Topic SelectedTopic
-		{
-			get { return _selectedTopic; }
-			set { _selectedTopic = value; }
-		}
-
-		public System.Windows.Visibility LogoutVisibility
-		{
-			get { return _logoutVisibility; }
-			private set 
-			{
-				_logoutVisibility = value;
-				NotifyOfPropertyChange(() => LogoutVisibility);
-			}
-		}
 
 
 		#endregion
@@ -102,73 +28,32 @@ namespace Rateit.ViewModels
 
 		public ShellViewModel()
 		{
-            //LoadParentCategories();
             AggregatorProvider.Aggregator.Subscribe(this);
 
 			ActivateItem(new LoginViewModel());
-
 		}
-
-        public void Logout()
-        {
-            User = null;
-			LogoutVisibility = System.Windows.Visibility.Hidden;
-            ActivateItem(new LoginViewModel());
-        }
 
 		#endregion
 
 		#region Private Methods
 
-		private void LoadParentCategories()
+
+
+		#endregion
+
+		#region Events
+
+		public void Handle(LoginEvent login)
 		{
-			ParentCategories.Clear();
-			List<Category> categories = Category.GetCategoriesByParent(null);
-			foreach (Category category in categories)
-			{
-				ParentCategories.Add(category);
-			}
+			ActivateItem(new MainViewModel(login.LoggedInUser));
 		}
 
-		private void LoadChildCategories()
+		public void Handle(LogoutEvent logout)
 		{
-			ChildCategories.Clear();
-            if (SelectedParentCategory != null)
-            {
-                List<Category> categories = Category.GetCategoriesByParent(SelectedParentCategory.Id);
-                foreach (Category category in categories)
-                {
-                    ChildCategories.Add(category);
-                }
-            }
+			ActivateItem(new LoginViewModel());
 		}
 
-		private void LoadTopics()
-		{
-			Topics.Clear();
-            if (SelectedChildCategory != null)
-            {
-                List<Topic> topics = Topic.GetTopicsByCategoryId(SelectedChildCategory.Id);
-                foreach (Topic topic in topics)
-                {
-                    Topics.Add(topic);
-                }
-            }
-		}
+		#endregion
 
-        #endregion
-
-        #region Events
-
-        public void Handle(LoginEvent message)
-        {
-            User = message.LoggedInUser;
-			LogoutVisibility = System.Windows.Visibility.Visible;
-			LoadParentCategories();
-            //TODO: ActivateItem(new RatingsViewModel);
-        }
-
-        #endregion
-
-    }
+	}
 }
